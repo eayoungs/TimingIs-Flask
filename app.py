d582d1489c46cec0f06cbada5dc428fcf965ea78
Eric Youngson <eric@successionecological.com>
2017-01-17 10:35:46 -0800

    Added templating and made buttons _live_

--- app.py
+++ app.py
@@ -12,10 +12,22 @@ from googleapiclient import discovery
 app = create_app()
 app.secret_key = str(uuid.uuid4())
 
+baseUrl = "http://127.0.0.1:5000/"
 
 @app.route('/')
 def main():
-  return render_template('index.html')
+  return render_template('template.html',
+                         homeBttnClass="active",
+                         homeUrl=baseUrl,
+                         aboutUrl=baseUrl+"about",
+                         contactUrl=baseUrl+"contact",
+                         quoteText='"This Moment is All There Is"',
+                         quoteAttrib='--Rumi',
+                         subheading1='Build your routine, your way',
+                         subtext1='Find out where your spend your time, setgoals and check in on your progress with minimal effort--all in from withing the calendar you already know & love: Google Calendar',
+                         subheading2="Show the muse you're serious",
+                         subtext2='You have to show up for your dreams; no one can do it for you but we can give you the tools to stay present')
+
 
 @app.route('/google_oauth2')
 def google_oauth2():
@@ -28,7 +40,7 @@ def google_oauth2():
 
     else: # https://developers.google.com/api-client-library/python/auth/web-app
       http_auth = credentials.authorize(httplib2.Http())
-      service = discovery.build('calendar', 'v3', http=http)
+      service = discovery.build('calendar', 'v3', http=http_auth)
       return render_template('g-oath2-landing.html')
     
 
@@ -52,6 +64,33 @@ def callback():
         session['credentials'] = credentials.to_json()
 
         return redirect(url_for('google_oauth2'))
+
+
+@app.route('/about')
+def about_page():
+  return render_template('template.html',
+                         aboutBttnClass="active",
+                         homeUrl=baseUrl,
+                         aboutUrl=baseUrl+"about",
+                         contactUrl=baseUrl+"contact",
+                         quoteText='About',
+                         quoteAttrib='',
+                         subheading1='Read-only parsing of your calendar data',
+                         subtext1="Timing.Is will not store your data. It will produce summary charts describing the amount and percent of of total for all unique events, by calendar or time spent in various categories determined by a 'tag'of your choosing, which can be any word or phrase that you want to use. Activity domains, such as physicalsocial, spiritual or mental; categorical markers, like professional, personal or communal. If you want tomodify the code and create your own custom algorithms, you can. Right there in the same page where theresults appear.")
+
+
+@app.route('/contact')
+def contact_page():
+  return render_template('template.html',
+                         contactBttnClass="active",
+                         homeUrl=baseUrl,
+                         aboutUrl=baseUrl+"about",
+                         contactUrl=baseUrl+"contact",
+                         quoteText='Contact',
+                         quoteAttrib='',
+                         subheading1='',
+                         subtext1="",)
+
 
 
 if __name__ == '__main__':
