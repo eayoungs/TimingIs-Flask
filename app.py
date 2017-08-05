cd53e4f999b0788b96e9af7ed5c6d64a17ca17c0
Eric Youngson <eric@successionecological.com>
2017-08-05 11:19:16 -0700

    Added checks & new default values for required arguments to invoice

--- app.py
+++ app.py
@@ -132,17 +132,26 @@ def google_oauth2():
                             billing_rate = form.billing_rate.data
                         else:
                             billing_rate = '1'
+                        if form.provider_tax_rate.data:
+                            provider_tax_rate = form.provider_tax_rate.data
+                        else:
+                            provider_tax_rate = 0
+                        if form.invoice_id.data:
+                            invoice_id=form.invoice_id.data
+                        else:
+                            invoice_id=1
                         invoice.main(invoiceItemsDct,
                                      billing_rate=billing_rate,
                                      provider_email=form.provider_email.data,
+                                     client_email=form.client_email.data,
                                      provider_name=form.provider_name.data,
                                      provider_street=form.provider_street.data,
                                      provider_city=form.provider_city.data,
                                      provider_state=form.provider_state.data,
                                      provider_country=form.provider_country.data,
                                      provider_post_code=form.provider_post_code.data,
-                                     #provider_tax_rate=form.provider_tax_rate.data,
-                                     #invoice_id=form.invoice_id.data
+                                     provider_tax_rate=provider_tax_rate,
+                                     invoice_id=invoice_id
                                      )
 
                         #calDursSmry = dfs.get_cals_durs(workTypesDct)
