679743aee1e2c85016121358d5a7b15d36d83c43
Eric Youngson <eayoungs@gmail.com>
2017-11-08 17:30:05 -0800

    Slowly splicing timging.py & views.py; included import statements with a few exceptions, minor changes

--- timingis/__init__.py
+++ timingis/__init__.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import (Flask, render_template, session, url_for, request, redirect,
+                   send_file)
+#from bootstrap_flask import create_app
+#from forms import CalendarSelectForm
+
+
 app = Flask(__name__)
 
 import timingis.views
--- timingis/views.py
+++ timingis/views.py
@@ -1,4 +1,28 @@
 from timingis import app
+from oauth2client.client import OAuth2WebServerFlow, OAuth2Credentials
+
+import re
+import uuid
+import json
+import os
+
+import httplib2
+from googleapiclient import discovery
+
+from geepal import get_events as ge
+from geepal import dfsort as dfs
+import invoice
+
+
+#app = Flask(__name__)#app = create_app()
+app.secret_key = str(uuid.uuid4())
+app.config.from_pyfile('settings.cfg')
+BASE_URL = app.config['BASE_URL']
+CLIENT_ID = app.config['CLIENT_ID']
+CLIENT_SECRET = app.config['CLIENT_SECRET']
+SCOPE = app.config['SCOPE']
+REDIRECT_URI = app.config['REDIRECT_URI']
+
 
 @app.route('/')
 def index():
